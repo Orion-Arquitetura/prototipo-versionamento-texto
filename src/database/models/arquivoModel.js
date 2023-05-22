04234f517927341bd4ed5b8ce511e13ea331dc3f
Orion-Arquitetura <gerenciadororionarquitetura@gmail.com>
2023-05-22 12:33:05 -0300

    criei o model para filtros e adicionei os possiveis valores para os campos "tipo", "disciplina" e "etapa"

--- src/database/models/arquivoModel.js
+++ src/database/models/arquivoModel.js
@@ -1,22 +1,63 @@
 const mongoose = require("mongoose");
 
 const arquivoSchema = mongoose.Schema({
-    nome: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    dataCriacao: {
-        type: Date,
-        default: Date.now
-    },
-    projeto: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Projeto",
-        required: true
-    }
-})
+  nome: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  dataCriacao: {
+    type: Date,
+    default: Date.now,
+  },
+  projeto: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Projeto",
+    required: true,
+  },
+  tipo: {
+    type: String,
+    required: true,
+    enum: ["MED", "CAD", "ORC", "CRO", "SIN", "ANA", "LSD", "LSM", "LSP", "COT"],
+  },
+  disciplina: {
+    type: String,
+    required: true,
+    enum: [
+      "ACS",
+      "LOG",
+      "ARQ",
+      "MEC",
+      "PVI",
+      "ORC",
+      "ESG",
+      "PDA",
+      "HID",
+      "PSG",
+      "CRO",
+      "EST",
+      "DEC",
+      "FND",
+      "ELE",
+      "SDG",
+      "GAS",
+      "TER",
+      "IES",
+      "TOP",
+      "SIP",
+      "IMP",
+      "SEL",
+      "LMT",
+    ],
+  },
+  etapa: {
+    type: String,
+    required: true,
+    enum: ["LV", "EP", "AP", "PB", "PE"],
+  },
+});
 
-const Arquivo = mongoose.models.Arquivo || mongoose.model("Arquivo", arquivoSchema, "Arquivos")
+const Arquivo =
+  mongoose.models.Arquivo || mongoose.model("Arquivo", arquivoSchema, "Arquivos");
 
-export default Arquivo
+export default Arquivo;
