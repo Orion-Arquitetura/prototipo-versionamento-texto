48ec85612e1b4dc109cf3c62f304ac72ae061fc3
Orion-Arquitetura <gerenciadororionarquitetura@gmail.com>
2023-05-23 13:39:45 -0300

    deleted

--- src/pages/api/dbtest/deleteSchema.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-import mongoose from "mongoose";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-  console.log("ok")
-  res.status(200).end();
-}
