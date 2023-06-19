c4065065d3cc11d4812097357dd2c487427b3107
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-06-19 16:44:46 -0300

    added users and files count. now uses MUI grid

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -3,34 +3,32 @@ import Link from "next/link";
 import React from "react";
 import GroupIcon from "@mui/icons-material/Group";
 import FolderIcon from "@mui/icons-material/Folder";
+import { Grid } from "@mui/material";
 
-const StyledLi = styled.li`
-  flex: 0 0 calc(33.33% - 10px);
-  height: 100px;
-  border-radius: 3px;
-  background-color: var(--midnight-green);
-  color: #ffffffdc;
-  transition: background-color 0.2s ease, color 0.2s ease;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.3);
-  display: flex;
-  position: relative;
-
-  &:hover,
-  &:focus,
-  &:focus-within {
-    background-color: var(--gray1);
-    background-image: url("/orion-estrela.png");
-    background-size: 155px;
-    background-repeat: no-repeat;
-    background-position-x: 120%;
-    background-position-y: 50%;
-    color: #1d424d;
-  }
-
+const StyledGridItem = styled(Grid)`
   .widget-project-data-div {
     height: 100%;
     display: flex;
     width: 100%;
+    height: 100px;
+    border-radius: 3px;
+    background-color: var(--midnight-green);
+    color: #ffffffdc;
+    transition: background-color 0.2s ease, color 0.2s ease;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.3);
+    position: relative;
+
+    &:hover,
+    &:focus,
+    &:focus-within {
+      background-color: var(--gray1);
+      background-image: url("/orion-estrela.png");
+      background-size: 155px;
+      background-repeat: no-repeat;
+      background-position-x: 120%;
+      background-position-y: 50%;
+      color: #1d424d;
+    }
 
     .project-data-icons {
       position: absolute;
@@ -59,20 +57,30 @@ const StyledLi = styled.li`
 type WidgetData = {
   title: string;
   link: string;
+  projectData: { usuarios: string[]; arquivos: string[] };
 };
 
-export default function Widget({ title, link }: WidgetData) {
+export default function Widget({ title, link, projectData }: WidgetData) {
   return (
-    <StyledLi>
+    <StyledGridItem
+      item
+      xs={2}
+    >
       <div className="widget-project-data-div">
-        <Link href={{
-          pathname: link
-        }} >{title}</Link>
+        <Link
+          href={{
+            pathname: link,
+          }}
+        >
+          {title}
+        </Link>
         <div className="project-data-icons">
           <FolderIcon fontSize="small" />
+          <span>{projectData.arquivos.length}</span>
           <GroupIcon fontSize="small" />
+          <span>{projectData.usuarios.length}</span>
         </div>
       </div>
-    </StyledLi>
+    </StyledGridItem>
   );
 }
--- src/components/WidgetBox.tsx
+++ src/components/WidgetBox.tsx
@@ -1,26 +1,14 @@
-import styled from "@emotion/styled";
+import { Grid } from "@mui/material";
 
-type BoxProps = {
-  direction: "column" | "row";
-  children: any;
-};
-
-const StyledDiv = styled.ul<BoxProps>`
-  min-width: 100%;
-  min-height: 100%;
-  display: flex;
-  flex-direction: ${(props) => props.direction};
-  justify-content: space-between;
-  flex-wrap: wrap;
-  row-gap: 20px;
-  column-gap: 15px;
-  margin-top: 20px;
-
-  & > li {
-    max-width: ${(props) => (props.direction === "column" ? "30%" : "100%")};
-  }
-`;
-
-export default function WidgetBox({ direction = "row", children }: BoxProps) {
-  return <StyledDiv direction={direction}>{children}</StyledDiv>;
+export default function WidgetBox({ children }: any) {
+  return (
+    <Grid
+      container
+      columns={6}
+      mt={2}
+      columnSpacing={5}
+    >
+      {children}
+    </Grid>
+  );
 }
