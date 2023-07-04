a94946c56401269d740ffd01da8fbfd8089cff20
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-07-04 12:44:16 -0300

    remove all files registers from the projects documents

--- src/pages/api/dbtest/deleteAllFiles.ts
+++ src/pages/api/dbtest/deleteAllFiles.ts
@@ -1,9 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import mongoose from "mongoose";
+import Projeto from "@/database/models/projectModel";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await mongoose.connection.collection("Arquivos").deleteMany({});
+  await Projeto.updateMany({}, {$unset: {"arquivos": ""}})
 
   res.status(200).end();
 }
