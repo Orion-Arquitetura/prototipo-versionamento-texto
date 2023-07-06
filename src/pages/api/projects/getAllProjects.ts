548b053c6835dc0511b79da8e4697b46ca1762f5
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-07-06 19:52:29 -0300

    filtering user allowed projects in the backend

--- src/pages/api/projects/getAllProjects.ts
+++ src/pages/api/projects/getAllProjects.ts
@@ -1,21 +1,34 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Projeto from "@/database/models/projectModel";
+import { parseCookies } from "nookies";
 import connectToDatabase from "@/database/mongodbConnection";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await connectToDatabase("App");
+  connectToDatabase("App")
+  
+  const { "user-permissoes": stringPermissoes, "user-tipo": tipo } = parseCookies({
+    req,
+  });
 
-  if (req.query.id) {
-    const ids = req.query.id;
-    const data = await Projeto.find({ _id: { $in: ids } })
-      .exec()
-      .then((res) => res);
-    res.status(200).json(data);
-  } else {
+  if (tipo === "administrador") {
     const data = await Projeto.find()
-      .exec()
-      .then((res) => res);
-
+    .exec()
+    .then((res) => res);
+    
     res.status(200).json(data);
+    return;
   }
+
+  console.log(stringPermissoes, tipo)
+  
+  const permissoes = JSON.parse(stringPermissoes);
+  
+  const projectsIds = permissoes.projetos.map((item:any) => item.id)
+  
+  const data = await Projeto.find({ _id: { $in: projectsIds } })
+    .exec()
+    .then((res) => res);
+  res.status(200).json(data);
+  return;
+  // res.status(200).end()
 }
