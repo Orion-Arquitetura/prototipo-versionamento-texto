5c51f1e920a84b0b62f8c66fdd6c6e845fc11caf
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-07-11 20:00:19 -0300

    changed "permissoes" for "projetos". Added "tarefas"

--- src/pages/api/user/auth.ts
+++ src/pages/api/user/auth.ts
@@ -27,7 +27,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         tipo: user.tipo.toLowerCase(),
         id: user._id,
         token: uuid(),
-        permissoes: user.permissoes
+        projetos: user.projetos,
+        tarefas: user.tarefas
       });
     }
 
