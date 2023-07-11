fd5f3e4de6c235b385da66f1bfc3c8fd64c8f8e0
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-07-11 19:59:10 -0300

    updated "Arquivo" interface

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -78,6 +78,7 @@ export interface Projeto {
 export interface Arquivo {
   _id: string;
   nome: string;
+  dataCriacao: string;
   tipo: string;
   disciplina: string;
   etapa: string;
@@ -85,8 +86,20 @@ export interface Arquivo {
   projeto: [{ nome: string; id: string }];
   versao: 0;
   ultimaVersao: boolean;
-  criadoPor: [{ userName: string; userId: string }];
-  dataCriacao: string;
+  novaVersaoSolicitada?: boolean;
+  responsavelNovaVersao?: {
+    nome: string;
+    id: string;
+  };
+  prazoNovaVersao?: string;
+  emRevisao?: boolean;
+  responsavelRevisao?: {
+    nome: string;
+    id: string;
+  };
+  prazoRevisao?: string;
+  comentario: string;
+  criadoPor: { userName: string; userId: string };
 }
 
 export interface signInData {
@@ -101,4 +114,64 @@ export interface AuthContextUserData {
   id: string;
   token: string;
   projetos: [{ nome: string; id: string }];
+  tarefas: {
+    concluidas: {
+      revisao: [
+        {
+          arquivo: {
+            nome: string;
+            id: string;
+          };
+          dataConclusao: string;
+          prazo: string;
+          projeto: {
+            nome: string;
+            id: string;
+          };
+        }
+      ];
+      novaVersao: [
+        {
+          arquivo: {
+            nome: string;
+            id: string;
+          };
+          dataConclusao: string;
+          prazo: string;
+          projeto: {
+            nome: string;
+            id: string;
+          };
+        }
+      ];
+    };
+    emAndamento: {
+      revisao: [
+        {
+          arquivo: {
+            nome: string;
+            id: string;
+          };
+          prazo: string;
+          projeto: {
+            nome: string;
+            id: string;
+          };
+        }
+      ];
+      novaVersao: [
+        {
+          arquivo: {
+            nome: string;
+            id: string;
+          };
+          prazo: string;
+          projeto: {
+            nome: string;
+            id: string;
+          };
+        }
+      ];
+    };
+  };
 }
