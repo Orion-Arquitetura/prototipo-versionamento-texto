14d3342d221702fd00526dddbb0cafeacacfe68c
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-09-08 11:57:01 -0300

    atualiza arquivo anterior como "ultimaVersao": true

--- src/pages/api/arquivos/deleteFile.ts
+++ src/pages/api/arquivos/deleteFile.ts
@@ -4,13 +4,23 @@ import mongoose from "mongoose";
 import { NextApiRequest, NextApiResponse } from "next";
 import { parseCookies } from "nookies";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { fileID, projectID } = req.query;
 
   const { bucket } = await connectToDatabase();
+
+  const arquivosCollection = mongoose.connection.collection("Arquivos.files");
+
+  const arquivo = await arquivosCollection.findOne({
+    _id: new mongoose.Types.ObjectId(fileID as string),
+  });
+
+  await arquivosCollection.updateOne(
+    { "metadata.versao": arquivo!.metadata.versao - 1 },
+    {
+      $set: { "metadata.ultimaVersao": true },
+    }
+  );
 
   await bucket.delete(new mongoose.Types.ObjectId(fileID as string));
 
