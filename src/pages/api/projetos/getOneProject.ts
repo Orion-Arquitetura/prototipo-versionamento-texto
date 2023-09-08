d9b43744651b2b37f2da86ed80e8e25df141dc88
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-09-08 11:56:37 -0300

    remove then

--- src/pages/api/projetos/getOneProject.ts
+++ src/pages/api/projetos/getOneProject.ts
@@ -10,6 +10,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       select: "-senha",
     })
     .exec()
-    .then((res) => res);
+    
   res.status(200).json(data);
 }
