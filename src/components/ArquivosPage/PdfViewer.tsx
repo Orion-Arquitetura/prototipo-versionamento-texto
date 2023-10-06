94fedc0e3a0b1519688577a286a8547d4817debf
Jp-Bruno <jp.bruno.machado@gmail.com>
2023-10-06 17:25:15 -0300

    pdf view

--- src/components/ArquivosPage/PdfViewer.tsx
+++ src/components/ArquivosPage/PdfViewer.tsx
@@ -2,29 +2,40 @@ import styled from "@emotion/styled";
 
 const StyledDiv = styled("div")`
   height: 100%;
+  position: relative;
 
   & iframe {
     width: 100%;
     height: 100%;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 43%;
+    height: 59px;
+    background-color: rgb(50, 54, 57);
+    right: 2px;
+    top: 2px;
+    z-index: 100;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 42%;
+    height: 59px;
+    background-color: rgb(50, 54, 57);
+    left: 2px;
+    top: 2px;
+    z-index: 100;
   }
 `;
 
 export default function PdfViewer({ url }: any) {
   return (
     <StyledDiv>
-      <iframe src={`${url}#toolbar=0`} />
+      <iframe src={`${url}#toolbar=1`} />
     </StyledDiv>
   );
 }
-
-// import { MapContainer } from "react-leaflet";
-// import { TileLayer } from 'react-leaflet';
-
-// export default function PdfViewer({ url }: any) {
-//   return (
-//     <MapContainer center={[0, 0]} zoom={1} scrollWheelZoom={false} >
-//       <TileLayer url={url} />
-
-//     </MapContainer>
-//   )
-// }
